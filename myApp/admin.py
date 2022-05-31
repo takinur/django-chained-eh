0bd685ab317ca0c21292a125e14823d672b0d44d
TAKINUR <takinurm@gmail.com>
2022-05-31 18:22:46 +0600

    added automated total price and slug field

--- myApp/admin.py
+++ myApp/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from myApp.models import product
 
-admin.site.register(product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product_name', 'detials', 'quantity', 'unit_price', 'total_price', 'purchase_date']
+    prepopulated_fields = {'slug': ('product_name',),}
+    exclude = ('total_price', )
+
+admin.site.register(product, ProductAdmin)
 # Register your models here.
--- myApp/models.py
+++ myApp/models.py
@@ -1,15 +1,25 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
 class product(models.Model):
     product_name = models.CharField(max_length=50)
+    slug = models.SlugField(null=True, blank=True)
     detials = models.CharField(max_length=50, null=True)
     unit_price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.IntegerField(default=0)
     total_price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     purchase_date = models.DateTimeField(auto_now_add=True)
     
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.product_name)
+        self.total_price = self.unit_price * self.quantity
+        super(product, self).save(*args, **kwargs)
+    
+    
     def __str__(self) -> str:
         return self.product_name
-        
+    
+    
