3655df3651aecbe88dbe9a0931f150c42d01023e
TAKINUR <takinurm@gmail.com>
2022-06-07 18:07:18 +0600

    Added product Category

--- myApp/admin.py
+++ myApp/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 
-from myApp.models import product
+from myApp.models import Category, Product
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    
+    prepopulated_fields = {'slug': ('category_name',)}
+    
+    list_display = ('category_name', 'slug', 'details')
+    
+    class Meta:
+        model = Product
+        
+admin.site.register(Category, CategoryAdmin)
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'product_name', 'detials', 'quantity', 'unit_price', 'total_price', 'purchase_date']
+    list_display = ['id', 'product_name', 'details', 'quantity', 'unit_price', 'total_price', 'purchase_date']
     prepopulated_fields = {'slug': ('product_name',),}
     exclude = ('total_price', )
 
-admin.site.register(product, ProductAdmin)
+admin.site.register(Product, ProductAdmin)
 # Register your models here.
--- myApp/models.py
+++ myApp/models.py
@@ -3,20 +3,44 @@ from django.utils.text import slugify
 
 # Create your models here.
 
-class product(models.Model):
+class Category(models.Model):
+
+    category_name = models.CharField(max_length=50)
+    slug = models.SlugField(null=True, blank=True)
+    details = models.CharField(max_length=50, null=True)
+    
+      
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.category_name)
+        super(Category, self).save(*args, **kwargs)
+    
+    def __str__ (self):
+          return self.category_name
+      
+class Product(models.Model):
     product_name = models.CharField(max_length=50)
     slug = models.SlugField(null=True, blank=True)
-    detials = models.CharField(max_length=50, null=True)
+    details = models.CharField(max_length=50, null=True)
     unit_price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.IntegerField(default=0)
     total_price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     purchase_date = models.DateTimeField(auto_now_add=True)
+    # images = models.ImageField(upload_to='photos/products')
+    images = models.FileField(upload_to='photos/products')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now=True)
     
     
     def save(self, *args, **kwargs):
         self.slug = slugify(self.product_name)
         self.total_price = self.unit_price * self.quantity
-        super(product, self).save(*args, **kwargs)
+        super(Product, self).save(*args, **kwargs)
+        
+    def delete(self, using=None, keep_parents=False):
+        if(self.images.name != 'photos/products/blank.png'):
+            self.images.storage.delete(self.images.name)
+        super().delete()
     
     
     def __str__(self) -> str:
